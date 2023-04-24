d2b15f6389abb0e886da8b0fea5f70467c6369cd
SangTin <sang080304@gmail.com>
2023-04-24 19:49:33 +0700

    Game Main Code

---
+++ src/MyAdventure.cpp
@@ -0,0 +1,37 @@
+#include <Core/Base.hpp>
+#include <Core/Game.hpp>
+
+const int FPS = 144;
+const int FPSTime = 1000.0 / FPS;
+
+int main(int argc, char* argv[]){
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << '\n';
+        return -1;
+    }
+
+    //Initialize PNG loading
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)){
+        std::cout << "IMG could not initialize! SDL_image Error: " << IMG_GetError() << '\n';
+        return -1;
+    }
+
+    //Initialize SDL_mixer
+    if (Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 ){
+        std::cout << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << '\n';
+        return -1;
+    }
+
+    Game window("My Adventure", baseSquare * 30, baseSquare * 20);
+
+    while (window.is_running()){
+        window.handle_events();
+        window.update();
+        window.clear();
+        window.render();
+    }
+    window.close();
+    
+    return 0;
+}
